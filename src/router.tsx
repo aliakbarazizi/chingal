49e81a04d01cb8c108c22a0056cb537a21235cd3
Ali Akbar Azizi <aliakbar.azizi20@gmail.com>
2023-11-04 07:36:35 +0330

    feat: change route type

---
+++ src/router.tsx
@@ -0,0 +1,74 @@
+import {
+  Route,
+  useLocation,
+  Navigate,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Layout from "./layouts/Layout";
+import { useUser } from "./features/auth";
+import Login from "./pages/Login";
+import EditUser from "./pages/EditUser";
+import Users from "./pages/Users";
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route
+        path="/login"
+        element={
+          <RequireGuest>
+            <Login />
+          </RequireGuest>
+        }
+      />
+      <Route
+        path="/"
+        element={
+          <RequireAuth>
+            <Layout />
+          </RequireAuth>
+        }
+      >
+        <Route
+          path="/users"
+          handle={{
+            breadcrumb: () => <span>لیست کاربران</span>,
+          }}
+        >
+          <Route index element={<Users />} />
+          <Route path="create" element={<Users />} />
+          <Route
+            path=":userId"
+            element={<EditUser />}
+            handle={{
+              breadcrumb: () => <span>ویرایش کاربر</span>,
+            }}
+          />
+        </Route>
+      </Route>
+    </Route>,
+  ),
+);
+
+function RequireGuest({ children }: { children: JSX.Element }) {
+  const user = useUser();
+  const location = useLocation();
+
+  if (user) {
+    return <Navigate to="/" state={{ from: location }} replace />;
+  }
+
+  return children;
+}
+
+function RequireAuth({ children }: { children: JSX.Element }) {
+  const user = useUser();
+  const location = useLocation();
+
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return children;
+}
