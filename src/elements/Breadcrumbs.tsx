0d51f2f74ad1f2915bed70fbac7d07b1d5b463cc
Ali Akbar Azizi <aliakbar.azizi20@gmail.com>
2023-11-04 07:36:53 +0330

    feat: ad breadcrumbs

---
+++ src/elements/Breadcrumbs.tsx
@@ -0,0 +1,32 @@
+import { UIMatch, useMatches } from "react-router-dom";
+import ArrowLeft1 from "../assets/icons/outline/ArrowLeft1";
+import classNames from "classnames";
+
+export default function Breadcrumbs() {
+  const matches = useMatches() as UIMatch<
+    unknown,
+    { breadcrumb: () => string }
+  >[];
+
+  const crumbs = matches
+    .filter((match) => Boolean(match.handle?.breadcrumb))
+    // now map them into an array of elements, passing the loader
+    // data to each one
+    .map((match) => match.handle.breadcrumb());
+
+  return (
+    <div className="flex gap-1">
+      {crumbs.map((item, index) => (
+        <div
+          className={classNames(
+            "flex",
+            index !== crumbs.length - 1 && "text-surface-500",
+          )}
+        >
+          <ArrowLeft1 />
+          <h2>{item}</h2>
+        </div>
+      ))}
+    </div>
+  );
+}
